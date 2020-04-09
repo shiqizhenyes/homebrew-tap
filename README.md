dddc8566332699b61a37451775a8c8e5540f7871
Patrick Samson <patrick@ideesdefous.com>
2020-04-09 12:24:19 -0400

    Refactor from spotify-tui to spotifyd

--- README.md
+++ README.md
@@ -1,46 +1,17 @@
 # Store homebrew formulae here.
 
-## spotify-tui
+## Spotifyd
 
-On your machine
-
-```bash
-cd spotify-tui
-```
-
-Generate a release build
+It can be installed using :
 
 ```bash
-cargo build --release
-```
-
-Now create a tar for that new release binary
-
-```bash
-cd target/release
-tar -czf spotify-tui-v0.3.0.tar.gz spt
-```
-
-Ensure that version number matches exactly the Github release you will create AND the version number in this repo `Formula/spotify-tui.rb` - in this case `v0.3.0`.
-
-Upload that tar to a new Github release.
-
-Now generate a `sha` from the `tar` and add the `sha` to `Formula/spotify-tui.rb`.
-
-```bash
-shasum -a 256 spotify-tui-v0.3.0.tar.gz
-```
-
-You will now finally be able to install a new version using
-
-```bash
-brew install Rigellute/tap/spotify-tui
+brew install Lykegenes/tap/spotifyd
 ```
 
 ### Automated
 
-The binaries and sha256s are generated from Github Actions. Run the following script to fetch the latest sha256s and update `Formula/spotify-tui.rb`.
+The binaries and sha512s are already generated in the Spotifyd repository. Run the following script to fetch the latest sha512s and update `Formula/spotifyd.rb`.
 
 ```
-sh scripts/spotify-tui.sh $VERSION
+sh scripts/spotifyd.sh $VERSION
 ```
---
+++ scripts/spotifyd.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+VERSION=$1
+
+# check presence of version
+if [ -z $VERSION ]; then
+	echo "Missing argument with version" >&2
+	exit 1
+fi
+
+echo "Preparing release $VERSION"
+
+# check presence of tooling
+SHASUM=`which shasum`
+CURL=`which curl`
+[ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
+
+# Check if macOS release exists, if it does then it is likely that the Linux one does too
+TARGET="https://github.com/Spotifyd/spotifyd/releases/download/v${VERSION}/spotifyd-macos-full.tar.gz"
+
+CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${TARGET}`
+if [ $CHECKVER_CODE -ne 302 ]; then
+	echo "Version ${VERSION} does not exist" >&2
+	exit 3
+fi
+
+# The URLs for the respective sha512s
+LINUX_SHA_URL="https://github.com/Spotifyd/spotifyd/releases/download/v${VERSION}/spotifyd-linux-full.sha512"
+MAC_SHA_URL="https://github.com/Spotifyd/spotifyd/releases/download/v${VERSION}/spotifyd-macos-full.sha512"
+
+echo "Fetching Linux sha512"
+LINUX_SHA=$(curl -sLS "${LINUX_SHA_URL}" | cut -f1 -d\ "")
+echo "Fetching macOS sha512"
+MAC_SHA=$(curl -sLS "${MAC_SHA_URL}" | cut -f1 -d\ "")
+
+cat > Formula/spotifyd.rb <<FORMULA
+class SpotifyTui < Formula
+  version "v${VERSION}"
+  desc "A spotify daemon"
+  homepage "https://github.com/Spotifyd/spotifyd"
+
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/Spotifyd/spotifyd/releases/download/#{version}/spotify-tui-macos.tar.gz"
+    sha512 "${MAC_SHA}"
+  elsif OS.linux?
+    url "https://github.com/Spotifyd/spotifyd/releases/download/#{version}/spotify-tui-linux.tar.gz"
+    sha512 "${LINUX_SHA}"
+  end
+
+  depends_on "dbus"
+  depends_on "portaudio"
+
+  def install
+    bin.install "spotifyd"
+  end
+
+  test do
+    system "#{bin}/spotifyd", "--version"
+  end
+end
+FORMULA
+
+echo "Successfully updated! Check the code before pushing."
