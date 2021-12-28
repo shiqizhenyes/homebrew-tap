9ea927fd23d554f65fdbe56f314acfc63f0f991e
zack <shiqizhenyes@gmail.com>
2021-12-28 14:26:55 +0800

    modity nexus
    modity nexus

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -2,11 +2,11 @@ class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
   url "https://github.com/shiqizhenyes/nexus/releases/download/v3.34.0-01/nexus-3.34.0-01.zip"
-  sha256 "4604cf0f97a8c43a57e25f8bf65cfb54596414ad77de0ab20f7499b7937f80a9"
+  sha256 "f298d330cffc8fec9156c5f758dceed52f3a0ec9d7efd6bf205cf47bccec6d0f"
   license "EPL-1.0"
 
   def install
-    ENV.prepend_create_path "JAVA_HOME", libexec+".install4j/jre.bundle/Contents/Home"
+    ENV.prepend_create_path "JAVA_HOME", libexec+".install4j/jre.bundle/Contents/Home/jre"
     libexec.install Dir["*"]
     bin.install libexec/"bin/nexus"
     bin.env_script_all_files libexec/"bin", :JAVA_HOME => ENV["JAVA_HOME"]
@@ -16,7 +16,6 @@ class Nexus < Formula
     mkdir_p "#{var}/log/nexus" unless (var/"log/nexus").exist?
     mkdir_p "#{var}/nexus" unless (var/"nexus").exist?
     mkdir "#{etc}/nexus" unless (etc/"nexus").exist?
-    mkdir_p "#{share}/nexus/sonatype-work" unless (share/"nexus/sonatype-work").exist?
   end
 
   service do
