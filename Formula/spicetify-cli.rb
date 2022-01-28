2b1c2794375f313744b3cf4f700738bc923213d1
zack <shiqizhenyes@gmail.com>
2022-01-28 13:43:50 +0800

    add spicetify-cli
    add spicetify-cli

---
+++ Formula/spicetify-cli.rb
@@ -0,0 +1,20 @@
+class SpicetifyCli < Formula
+    version "v2.8.4"
+    desc "Commandline tool to customize Spotify client. Supports Windows, MacOS and Linux."
+    homepage "https://github.com/shiqizhenyes/spotifyd"
+  
+    if OS.mac?
+      url "https://github.com/shiqizhenyes/spicetify-cli/releases/download/#{version}/spicetify-darwin-amd64.tar.gz"
+      sha256 "6be3f1ac9effc2182bcab201e07b38cdb0fe7e7b742c681bfb2c69da32853a38"
+    end
+  
+    def install
+      libexec.install Dir["*"]
+      bin.install libexec/"spicetify"
+      bin.env_script_all_files libexec/"spicetify"
+    end
+  
+    test do
+      system "#{bin}/spicetify", "--version"
+    end
+  end
