5919abd88b3e7c554c151c4424d2d34056146c5d
zack <shiqizhenyes@gmail.com>
2023-08-01 21:25:17 +0800

    fix post install error
    fix post install error

--- Formula/subconverter.rb
+++ Formula/subconverter.rb
@@ -2,7 +2,7 @@ class Subconverter < Formula
   desc "Utility to convert between various proxy subscription formats"
   homepage "https://github.com/tindy2013/subconverter"
   url "https://github.com/shiqizhenyes/subconverter/releases/download/v0.7.2/subconverter-0.7.2.zip"
-  sha256 "1d68f26d8b0610d6fd536722f76302b8ba2ab696fee6093967f9bde5979088f5"
+  sha256 "026cd4dbfd5cd9436c8631672c9e4152ecbd823aba2194f87acae983b7a65c57"
   license "GPL-3.0"
 
   def install
@@ -11,12 +11,14 @@ class Subconverter < Formula
   end
 
   def post_install
-    mkdir_p "#{var}/cache/subconverter" unless (var/cache/"subconverter").exist?
-    mkdir "#{etc}/subconverter" unless (etc/"subconverter").exist?
+    # mkdir_p "#{var}/cache/subconverter" unless (var/cache/"subconverter").exist?
+    # mkdir "#{etc}/subconverter" unless (etc/"subconverter").exist?
+    pkgetc.install libexec/"base"
+    pkgetc.install libexec/"snippets"
     # mv libexec/"base" "#{etc}/subconverter/"
     # mv libexec/"config" "#{etc}/subconverter/"
     # mv libexec/"rules" "#{etc}/subconverter/"
-    mv libexec/"snippets" "#{etc}/subconverter/"
+    # mv libexec/"snippets" "#{etc}/subconverter/"
     # mv libexec/"cache" "#{var}/cache/subconverter/"
   end
 
