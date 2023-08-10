1584ed7ccbd0d6395df6540109158d19562a040d
zack <shiqizhenyes@gmail.com>
2023-08-10 11:39:45 +0800

    subconverter upgrade
    subconverter upgrade

--- Formula/subconverter.rb
+++ Formula/subconverter.rb
@@ -1,21 +1,21 @@
 class Subconverter < Formula
   desc "Utility to convert between various proxy subscription formats"
   homepage "https://github.com/tindy2013/subconverter"
-  url "https://github.com/shiqizhenyes/subconverter/releases/download/v0.7.2/subconverter-0.7.2.zip"
-  sha256 "665a90f4c2c336bd699fc603bbbffc26e373042737e7320dd7e752dadb2894e1"
+  url "https://github.com/shiqizhenyes/subconverter/releases/download/v0.7.2/subconverter_darwin64.tar.gz"
+  sha256 "e2a35005e756508e58cd68cf17fa7fc099066f8fde5342168bb4c69c4e01a4d1"
   license "GPL-3.0"
 
   def install
     libexec.install Dir["*"]
     # bin.install libexec/"bin/subconverter"
-    bin.write_exec_script (libexec/"bin/subconverter")
+    bin.write_exec_script (libexec/"subconverter")
   end
 
   def post_install
     # mkdir_p "#{var}/cache/subconverter" unless (var/cache/"subconverter").exist?
     # mkdir "#{etc}/subconverter" unless (etc/"subconverter").exist?
-    pkgetc.install libexec/"base"
-    pkgetc.install libexec/"snippets"
+    # pkgetc.install libexec/"base"
+    # pkgetc.install libexec/"snippets"
     mkdir_p "#{var}/log/subconverter" unless (var/"log/subconverter").exist?
     # mv libexec/"base" "#{etc}/subconverter/"
     # mv libexec/"config" "#{etc}/subconverter/"
